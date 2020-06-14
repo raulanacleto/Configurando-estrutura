37982ce74ec072f0760e162beb83dc902958709c
raulanacleto <raulanacletosilva@gmail.com>
2020-06-14 16:10:47 -0300

    trabalhando com dados

--- routes/appointments.routes.ts
+++ routes/appointments.routes.ts
@@ -20,10 +20,16 @@ appointmentsRouter.post('/', (request, response) => {
   const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
 
   if (findAppointmentInSameDate) {
-    return response.status(400).json({ message: 'this appointment is already booked' });
+    return response
+      .status(400)
+      .json({ message: 'this appointment is already booked' });
   }
 
-  const appointment = appointmentsRepository.create(provider, parsedDate);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
+
   return response.json(appointment);
 });
 
