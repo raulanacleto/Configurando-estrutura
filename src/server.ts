2d75b7e3e572536d7a809c0b547883ecb8dde675
raulanacleto <raulanacletosilva@gmail.com>
2020-06-14 14:58:48 -0300

    ajustes eslint

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,8 @@ import routes from '../routes';
 
 const app = express();
 
-app.use(routes)
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('server iniciado');
